e0ec8b624091a2cb24d32f9e7bbbd3e2995df0e6
Ghaith Salloum <ghaithsalloum12@gmail.com>
2026-07-22 21:56:32 +0300

    Add DVLD_Business and DVLD_DataAccess projects
    Added two new projects, DVLD_Business and DVLD_DataAccess, to the solution file. Updated DVLDProject.csproj to reference DVLD_Business.
    
    Created DVLD_Business.csproj as a .NET Framework 4.7.2 library with a basic Class1 implementation and metadata in AssemblyInfo.cs. Added a project reference to DVLD_DataAccess.
    
    Created DVLD_DataAccess.csproj as a .NET Framework 4.7.2 library with classes for database connectivity. Implemented clsDataAccessSettings for connection string management and clsPersonData for retrieving person details using ADO.NET.

---
+++ DVLD_DataAccess/clsPersonData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_DataAccess
+{
+    internal class clsPersonData
+    {
+        public static bool getPersonByID(int PersonID, ref string FirstName, ref string SecondName,
+          ref string ThirdName, ref string LastName, ref string NationalNo, ref DateTime DateOfBirth,
+           ref short Gendor, ref string Address, ref string Phone, ref string Email,
+           ref int NationalityCountryID, ref string ImagePath)
+        {
+
+
+
+            bool isFound = false;
+
+            using (SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString))
+            {
+                string query = @"SELECT * FROM Person WHERE PersonID = @PersonID";
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@PersonID", PersonID);
+                    try
+                    {
+                        connection.Open();
+
+                        using (SqlDataReader reader = cmd.ExecuteReader()) {
+
+                            if (reader.Read())
+                            {
+                                isFound = true;
+                                FirstName = reader["FirstName"].ToString();
+                                SecondName = reader["SecondName"].ToString(); 
+                                ThirdName = reader["ThirdName"] == DBNull.Value ? "" : reader["ThirdName"].ToString();
+                                LastName = reader["LastName"].ToString();
+                                DateOfBirth = Convert.ToDateTime(reader["DateOfBirth"]);
+                                Gendor = Convert.ToByte(reader["Gendor"]);
+                                Address = reader["Address"].ToString();
+                                Phone = reader["Phone"].ToString();
+                                Email = reader["Email"] == DBNull.Value ? "" : reader["Email"].ToString();
+                                NationalityCountryID = (int)reader["NationalityCountryID"];
+                                ImagePath = reader["ImagePath"] == DBNull.Value ? "" : reader["ImagePath"].ToString();
+                            }
+                            else
+                            {
+                                isFound = false;
+                            }
+                        
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return isFound;
+        }
+    
+
+
+    
+    
+    
+    
+    }
+}
